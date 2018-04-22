a6f835714cdc50f150d84a1a569c37680ffd579e
Ryan Petrich <rpetrich@gmail.com>
2018-04-22 12:44:45 -0400

    Switch to export default for main component instead of dom.host

--- app.tsx
+++ app.tsx
@@ -6,17 +6,15 @@ async function defaultComponent<PropsType>(modulePromise: Promise<{ default: dom
 	return dom.h((await modulePromise).default, props);
 }
 
-dom.host((
-	<div>
-		<h1>Mobius Sample App</h1>
-		<CollapsibleSection title="To Do List" contents={() => defaultComponent(import("./todo"), {})}/>
-		<CollapsibleSection title="Random Stream" contents={() => defaultComponent(import("./random"), {})}/>
-		<CollapsibleSection title="Messaging" contents={() => defaultComponent(import("./messaging"), {})}/>
-		<CollapsibleSection title="Weather" contents={() => defaultComponent(import("./weather"), {})}/>
-		<CollapsibleSection title="Session Sharing" contents={() => defaultComponent(import("./sharing"), {})}/>
-		<div class={about}>made with <span class={heart}>♥️</span> by <a href="https://twitter.com/rpetrich/">@rpetrich</a></div>
-	</div>
-));
+export default <div>
+	<h1>Mobius Sample App</h1>
+	<CollapsibleSection title="To Do List" contents={() => defaultComponent(import("./todo"), {})}/>
+	<CollapsibleSection title="Random Stream" contents={() => defaultComponent(import("./random"), {})}/>
+	<CollapsibleSection title="Messaging" contents={() => defaultComponent(import("./messaging"), {})}/>
+	<CollapsibleSection title="Weather" contents={() => defaultComponent(import("./weather"), {})}/>
+	<CollapsibleSection title="Session Sharing" contents={() => defaultComponent(import("./sharing"), {})}/>
+	<div class={about}>made with <span class={heart}>♥️</span> by <a href="https://twitter.com/rpetrich/">@rpetrich</a></div>
+</div>
 
 dom.title("Mobius Sample App");
 
