6058b4e40c1a59837c2ffcc664eb43783860a79f
Ryan Petrich <rpetrich@gmail.com>
2018-05-13 18:03:51 -0400

    Migrate WeatherResponse into weather.tsx now that we can do circular references for validators

--- weather.tsx
+++ weather.tsx
@@ -1,6 +1,6 @@
 import * as dom from "dom";
 import { parse, fromClient as fetchFromClient } from "fetch";
-import { WeatherResponse as isWeatherResponse } from "./yahoo!validators";
+import { WeatherResponse as isWeatherResponse } from "./weather!validators";
 
 export default class extends dom.Component<{}, { text: string }> {
 	async componentDidMount() {
@@ -18,4 +18,79 @@ export default class extends dom.Component<{}, { text: string }> {
 	render() {
 		return <span>{this.state.text || "Loading..."}</span>;
 	}
+}
+
+export interface WeatherResponse {
+	query: {
+		count: number;
+		created: string;
+		lang: string;
+		results: {
+			channel: {
+				astronomy: {
+					sunrise: string;
+					sunset: string;
+				};
+				atmosphere: {
+					humidity: string;
+					pressure: string;
+					rising: string;
+					visibility: string;
+				};
+				description: string;
+				image: {
+					height: string;
+					link: string;
+					title: string;
+					url: string;
+					width: string;
+				};
+				item: {
+					condition: {
+						code: string;
+						date: string;
+						temp: string;
+						text: string;
+					};
+					description: string;
+					forecast: {
+						code: string;
+						date: string;
+						day: string;
+						high: string;
+						low: string;
+					}[];
+					guid: {
+						isPermaLink: string;
+					};
+					lat: string;
+					link: string;
+					long: string;
+					pubDate: string;
+					title: string;
+				};
+				language: string;
+				lastBuildDate: string;
+				link: string;
+				location: {
+					city: string;
+					country: string;
+					region: string;
+				};
+				title: string;
+				ttl: string;
+				units: {
+					distance: string;
+					pressure: string;
+					speed: string;
+					temperature: string;
+				};
+				wind: {
+					chill: string;
+					direction: string;
+					speed: string;
+				};
+			};
+		};
+	};
 }
